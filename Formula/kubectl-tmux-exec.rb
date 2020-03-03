16fddce71dc4c66025064fc10333457ada84c9b9
Wenhao Ji <predator.ray@gmail.com>
2020-03-03 23:45:10 +0800

    Released kubectl-tmux-exec v0.0.2

--- Formula/kubectl-tmux-exec.rb
+++ Formula/kubectl-tmux-exec.rb
@@ -1,9 +1,9 @@
 class KubectlTmuxExec < Formula
     desc "A kubectl plugin that uses Tmux to multiplex commands to pods"
     homepage "https://github.com/predatorray/kubectl-tmux-exec"
-    url "https://github.com/predatorray/kubectl-tmux-exec/releases/download/v0.0.1/kubectl-tmux-exec-0.0.1.tar.gz"
-    version "0.0.1"
-    sha256 "4d83ac007e543d8afe749aa2c6e37ed48c21a2679ca6d3d3ba89a28194ffe204"
+    url "https://github.com/predatorray/kubectl-tmux-exec/releases/download/v0.0.2/kubectl-tmux-exec-0.0.2.tar.gz"
+    version "0.0.2"
+    sha256 "1699e634a19777ad8dc1e78e2f36893b11ca5c3cf0ac1f83f641af47d8060f81"
     depends_on "gnu-getopt"
     depends_on "tmux"
 
@@ -12,7 +12,7 @@ class KubectlTmuxExec < Formula
     def install
         prefix.install "LICENSE"
         libexec.install Dir["*"]
-        (bin/"kubectl-tmux-exec").write_env_script "#{libexec}/bin/kubectl-tmux-exec", :GNU_GETOPT_PREFIX => Formula["gnu-getopt"].opt_prefix
+        (bin/"kubectl-tmux-exec").write_env_script "#{libexec}/bin/kubectl-tmux_exec", :GNU_GETOPT_PREFIX => Formula["gnu-getopt"].opt_prefix
     end
 
     test do
