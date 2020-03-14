cc6d612e8e30ef532298c0a38de85787d7317bbb
Wenhao Ji <predator.ray@gmail.com>
2020-03-15 00:24:27 +0800

    published kubectl-tmux-exec v0.0.4

--- Formula/kubectl-tmux-exec.rb
+++ Formula/kubectl-tmux-exec.rb
@@ -1,9 +1,9 @@
 class KubectlTmuxExec < Formula
     desc "A kubectl plugin that uses Tmux to multiplex commands to pods"
     homepage "https://github.com/predatorray/kubectl-tmux-exec"
-    url "https://github.com/predatorray/kubectl-tmux-exec/releases/download/v0.0.3/kubectl-tmux-exec-0.0.3.tar.gz"
-    version "0.0.3"
-    sha256 "a373f96a5e57bba9545e824ed15a8cd9c9ddf00fdcc5357d3f69255d071f02ab"
+    url "https://github.com/predatorray/kubectl-tmux-exec/releases/download/v0.0.4/kubectl-tmux-exec-0.0.4.tar.gz"
+    version "0.0.4"
+    sha256 "cd5f475653031b1e408dd0d893c3c22592e89ac981142c41e62007bca56716bd"
     depends_on "gnu-getopt"
     depends_on "tmux"
 
