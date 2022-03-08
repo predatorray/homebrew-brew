5b00ed7b8c1269ceb8dced5777e192dae17ef135
Wenhao Ji <predator.ray@gmail.com>
2022-03-08 23:45:30 +0800

    fixed style issue

--- Formula/kubectl-tmux-exec.rb
+++ Formula/kubectl-tmux-exec.rb
@@ -1,8 +1,7 @@
 class KubectlTmuxExec < Formula
-  desc "The kubectl plugin that uses Tmux to multiplex commands to pods"
+  desc "Kubectl plugin that uses Tmux to multiplex commands to pods"
   homepage "https://github.com/predatorray/kubectl-tmux-exec"
-  url "https://github.com/predatorray/kubectl-tmux-exec/releases/download/v#{version}/kubectl-tmux-exec-#{version}.tar.gz"
-  version "0.3.0"
+  url "https://github.com/predatorray/kubectl-tmux-exec/releases/download/v0.3.0/kubectl-tmux-exec-0.3.0.tar.gz"
   sha256 "ed37019e56bac84362a255b7236d1d83e7b487a792f2689137ab5c75978f3009"
 
   depends_on "gnu-getopt"
@@ -12,9 +11,11 @@ class KubectlTmuxExec < Formula
     prefix.install "LICENSE"
     prefix.install "VERSION"
     libexec.install "bin/kubectl-tmux_exec"
-    (bin/"kubectl-tmux_exec").write_env_script "#{libexec}/kubectl-tmux_exec", :GNU_GETOPT_PREFIX => Formula["gnu-getopt"].opt_prefix
+    (bin/"kubectl-tmux_exec").write_env_script "#{libexec}/kubectl-tmux_exec",
+      GNU_GETOPT_PREFIX: Formula["gnu-getopt"].opt_prefix
   end
 
   test do
+    system "#{bin}/kubectl-tmux_exec", "--version"
   end
 end
