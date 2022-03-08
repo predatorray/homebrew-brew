771aa708a4847c9421eb1fcc8d71ef5fce2ac553
Wenhao Ji <predator.ray@gmail.com>
2022-03-09 00:18:35 +0800

    Remove the deprecated `bottle :unneeded` (#1)

--- Formula/kubectl-tmux-exec.rb
+++ Formula/kubectl-tmux-exec.rb
@@ -1,11 +1,9 @@
 class KubectlTmuxExec < Formula
-  desc "The kubectl plugin that uses Tmux to multiplex commands to pods"
+  desc "Kubectl plugin that uses Tmux to multiplex commands to pods"
   homepage "https://github.com/predatorray/kubectl-tmux-exec"
   url "https://github.com/predatorray/kubectl-tmux-exec/releases/download/v0.3.0/kubectl-tmux-exec-0.3.0.tar.gz"
-  version "0.3.0"
   sha256 "ed37019e56bac84362a255b7236d1d83e7b487a792f2689137ab5c75978f3009"
 
-  bottle :unneeded
   depends_on "gnu-getopt"
   depends_on "tmux"
 
@@ -13,9 +11,11 @@ class KubectlTmuxExec < Formula
     prefix.install "LICENSE"
     prefix.install "VERSION"
     libexec.install "bin/kubectl-tmux_exec"
-    (bin/"kubectl-tmux_exec").write_env_script "#{libexec}/kubectl-tmux_exec", :GNU_GETOPT_PREFIX => Formula["gnu-getopt"].opt_prefix
+    (bin/"kubectl-tmux_exec").write_env_script "#{libexec}/kubectl-tmux_exec",
+      GNU_GETOPT_PREFIX: Formula["gnu-getopt"].opt_prefix
   end
 
   test do
+    system "#{bin}/kubectl-tmux_exec", "--version"
   end
 end
