470e4805e97fbc15a96b165e727a137e0060725f
Wenhao Ji <predator.ray@gmail.com>
2026-03-01 15:01:10 -0800

    Bump kubectl-tmux-exec to v0.5.0 (#6)

--- Formula/kubectl-tmux-exec.rb
+++ Formula/kubectl-tmux-exec.rb
@@ -1,22 +1,21 @@
 class KubectlTmuxExec < Formula
   desc "Kubectl plugin that uses Tmux to multiplex commands to pods"
   homepage "https://github.com/predatorray/kubectl-tmux-exec"
-  url "https://github.com/predatorray/kubectl-tmux-exec/releases/download/v0.4.0/kubectl-tmux-exec-0.4.0.tar.gz"
-  sha256 "f82c498432f02a7ff619546733011360426a16190a122f849a9a8a82e795ee1e"
+  url "https://github.com/predatorray/kubectl-tmux-exec/releases/download/v0.5.0/kubectl-tmux-exec-0.5.0.tar.gz"
+  sha256 "6083d9724c028a0a70fa7266fc55518a105921e5204357c4b7e5616dbcf15739"
+  license "MIT"
 
-  depends_on "gnu-getopt"
   depends_on "tmux"
 
   def install
     prefix.install "LICENSE"
     prefix.install "VERSION"
     libexec.install "bin/kubectl-tmux_exec"
-    (bin/"kubectl-tmux_exec").write_env_script "#{libexec}/kubectl-tmux_exec",
-      GNU_GETOPT_PREFIX: Formula["gnu-getopt"].opt_prefix
+    bin.install_symlink libexec/"kubectl-tmux_exec"
     bin.install "bin/kubectl_complete-tmux_exec"
   end
 
   test do
-    system "#{bin}/kubectl-tmux_exec", "--version"
+    system bin/"kubectl-tmux_exec", "--version"
   end
 end
